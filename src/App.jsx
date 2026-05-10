6b90c5531b34aed5257c7279960ae5074b092287
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-05-10 14:39:17 +0200

    Logica filtraggio in base al genere

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 
 
 function App() {
-  const movies =  [
+  const moviesData =  [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
   { title: 'Titanic', genre: 'Romantico' },
@@ -9,30 +10,46 @@ function App() {
   { title: 'Interstellar', genre: 'Fantascienza' },
   { title: 'Pulp Fiction', genre: 'Thriller' },
  ]
- 
+
+  const [selectedGenre, setSelectedGenre] = useState('Tutti');
+
+  
+  const handleGenreChange = (event) => {
+    setSelectedGenre(event.target.value);
+  };
+
+  // Logica di filtraggio
+  const filteredMovies = selectedGenre === 'Tutti' 
+    ? moviesData 
+    : moviesData.filter(movie => movie.genre === selectedGenre);
+
  
  
 
   return (
     <>
-      <section>
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Select movie genre</option>
-          <option value="Fantascienza">Fantascienza</option>
-          <option value="Thriller">Thriller</option>
-          <option value="Romantico">Romantico</option>
-          <option value="Azione">Azione</option>
-        </select>
-        <hr />
-      </section>
-      <section>
-        <h2>Movie list</h2>
-        {movies.map(movie => (
-        <ul>
-          <li key={movie.index}>{movie.title} {movie.genre}</li>
-        </ul>
+<div>
+      <h1>Lista Filmi</h1>
+      
+      
+      <select onChange={handleGenreChange} value={selectedGenre}>
+        <option value="Tutti">Tutti i generi</option>
+        <option value="Azione">Azione</option>
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Thriller">Thriller</option>
+      </select>
+
+      
+      <ul>
+        {filteredMovies.map(movie => (
+          <li key={movie}>{movie.title} ({movie.genre})</li>
         ))}
-      </section>
+      </ul>
+    </div>
+
+
+     
     </>
   )
 }
